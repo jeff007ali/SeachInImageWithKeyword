d80c8504fb8c5c57ed4356e732b120c1fd2dceeb
jeff007ali <jagharia@gmail.com>
2020-02-02 14:03:09 +0530

    Removed newline character from EXIF data and converted all EXIF data text into lowercase to handle search more efficiently

--- SeachInImageWithKeyword.py
+++ SeachInImageWithKeyword.py
@@ -7,27 +7,33 @@ import shutil
 import pyexiv2
 
 currentPath = os.getcwd()
-print ("The current working directory is %s" % currentPath)
+print ("The current working directory is {}".format(currentPath))
 
 path = input("Enter images folder path : ")
 # change the current working directory to specified path
 os.chdir(path)
 
-keyword = input("Enter search keyword : ")
+keyword = input("Enter search keyword : ").lower()
 
 outputPath = "%s/output_%s" % (path, keyword)
 try:
     os.mkdir(outputPath)
 except OSError:
-    print ("Creation of the directory %s failed" % outputPath)
+    print ("Creation of the directory {} failed".format(outputPath))
 else:
-    print ("Successfully created the directory %s " % outputPath)
+    print ("Successfully created the directory {}".format(outputPath))
 
 for file in glob.glob("*.png"):
     # To get exif data using pyexiv library we have to create image object
     exiv_image = pyexiv2.Image(file)
+
+    # To clear exif data of all images
+    if keyword == "clear exif data":
+        exiv_image.clear_exif()    
+        continue
+
+    # Read EXIF data of image
     exif_data = exiv_image.read_exif()
-    
     # Check if exif data of text_from_image is present or not
     if exif_data.get('Exif.Photo.UserComment'):
         text_from_image = exif_data.get('Exif.Photo.UserComment')
@@ -38,6 +44,7 @@ for file in glob.glob("*.png"):
         # no need to create Image object.
         # TODO : Check the impact of directly passing image name in argument over image object
         text_from_image = pytesseract.image_to_string(pillow_image, lang="eng")
+        text_from_image = text_from_image("\n", "").lower()
 
         # TODO : Add text_from_image as dictionary not as direct string for future enhancement
         exif_data['Exif.Photo.UserComment'] = text_from_image
@@ -47,13 +54,16 @@ for file in glob.glob("*.png"):
         try:
             shutil.copy(file, outputPath)
         except IOError as e:
-            print("Unable to copy file. %s" % e)
+            print("Unable to copy file. {}".format(e))
             exit(1)
         except:
             print("Unexpected error:", sys.exc_info())
             exit(1)
 
-print("Done with searching")
+if keyword == "clear exif data":
+    print("EXIF data of all images are deleted")
+else: 
+    print("Done with searching")
 
 
 # see exiv2’s documentation for a list of valid EXIF tags : https://exiv2.org/tags.html
