3311d33248049e8a592dd7b8cc811c4cd73a2854
jeff007ali <jagharia@gmail.com>
2020-02-02 12:27:28 +0530

    Complete TO DO : If image is processed once then store retrieved text as EXIF data, it will reduce processing time of image

--- SeachInImageWithKeyword.py
+++ SeachInImageWithKeyword.py
@@ -4,14 +4,16 @@ from PIL import Image
 import pytesseract
 import glob, os
 import shutil
+import pyexiv2
 
 currentPath = os.getcwd()
 print ("The current working directory is %s" % currentPath)
 
-path = input("Enter folder path : ")
+path = input("Enter images folder path : ")
+# change the current working directory to specified path
 os.chdir(path)
 
-keyword = input("Enter keyword : ")
+keyword = input("Enter search keyword : ")
 
 outputPath = "%s/output_%s" % (path, keyword)
 try:
@@ -22,11 +24,26 @@ else:
     print ("Successfully created the directory %s " % outputPath)
 
 for file in glob.glob("*.png"):
-    im = Image.open(file)
-    text = pytesseract.image_to_string(im, lang="eng")
-    #TO DO : If image is processed once then store retrieved text in JSON file, it will reduce processing time of image.
+    # To get exif data using pyexiv library we have to create image object
+    exiv_image = pyexiv2.Image(file)
+    exif_data = exiv_image.read_exif()
+    
+    # Check if exif data of text_from_image is present or not
+    if exif_data.get('Exif.Photo.UserComment'):
+        text_from_image = exif_data.get('Exif.Photo.UserComment')
+    
+    else:
+        pillow_image = Image.open(file)
+        # image_to_string() this function can work if we pass image name directly
+        # no need to create Image object.
+        # TODO : Check the impact of directly passing image name in argument over image object
+        text_from_image = pytesseract.image_to_string(pillow_image, lang="eng")
 
-    if(keyword in text):
+        # TODO : Add text_from_image as dictionary not as direct string for future enhancement
+        exif_data['Exif.Photo.UserComment'] = text_from_image
+        exiv_image.modify_exif(exif_data)
+
+    if(keyword in text_from_image):
         try:
             shutil.copy(file, outputPath)
         except IOError as e:
@@ -37,3 +54,6 @@ for file in glob.glob("*.png"):
             exit(1)
 
 print("Done with searching")
+
+
+# see exiv2’s documentation for a list of valid EXIF tags : https://exiv2.org/tags.html
